fab1502b30689d8e15024bb14c5b9d352f2387cf
Jordan_M_Wofford <jmwofford@gmail.com>
2019-07-07 22:09:33 -0500

    intro: finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def index
+
+  end
+
+  def create
+    user = User.find_by(email:login_params[:email])
+      if user && user.authenticate(login_params[:password])
+        session[:user_id] = user.id
+        redirect_to '/dashboard'
+      else
+        flash[:login_errors] = ['invalid credentials']
+        redirect_to '/'
+      end
+  end
+
+
+  private
+    def login_params
+      params.require(:login).permit(:email,:password)
+    end
 end
