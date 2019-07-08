dd8510e5cdf809258b12b361924b78dadb029a05
Jordan_M_Wofford <jmwofford@gmail.com>
2019-07-08 10:05:43 -0500

    landingPage Set:

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/' => 'users#index'
+  get '/' => 'users#new'
+  get '/sessions' => 'sessions#index'
+  get '/home' => 'users#index'
   post '/sessions' => 'sessions#create'
   post '/users' => 'users#create'
   get '/dashboard' => 'profiles#index'
-
 end
