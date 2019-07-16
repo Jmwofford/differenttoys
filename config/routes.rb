3e5dee169d7afcf2b55d928d25464d9d61f7cd86
Jordan_M_Wofford <jmwofford@gmail.com>
2019-07-16 10:58:04 -0500

    switched index/new:

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/' => 'users#new'
+  get '/' => 'users#index'
   get '/sessions' => 'sessions#index'
-  get '/home' => 'users#index'
+  get '/home' => 'users#new'
   post '/sessions' => 'sessions#create'
   post '/users' => 'users#create'
   get '/dashboard' => 'profiles#index'
