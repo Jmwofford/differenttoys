7114710bcba29c08b8f05988fb1131f02590c480
Jordan_M_Wofford <jmwofford@gmail.com>
2019-07-17 13:55:35 -0500

    added Comments :

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'users#index'
+  get '/comments' => 'comments#index'
   get '/sessions' => 'sessions#index'
   get '/home' => 'users#new'
   post '/sessions' => 'sessions#create'
